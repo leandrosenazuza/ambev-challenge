bfb534108f5042b6d7e623600aa6ee0c9f8aae26
Leandro Zuza <lendro.zuza@connectbr.dev>
2025-04-12 00:17:41 -0300

    Fixed mapper to get User

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Mappings/GetUserProfile.cs
@@ -0,0 +1,17 @@
+﻿using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+using Ambev.DeveloperEvaluation.Application.Users.GetUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.CreateUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings
+{
+    public class GetUserProfileRequest : Profile
+    {
+            public GetUserProfileRequest()
+            {
+                CreateMap<GetUserResult, GetUserResponse>();
+            }
+        
+    }
+}

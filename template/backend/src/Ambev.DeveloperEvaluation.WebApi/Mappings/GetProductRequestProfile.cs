e288ad2c9f5e73e209ac9ce3d1e449557d629fe2
Leandro Zuza <lendro.zuza@connectbr.dev>
2025-04-13 23:54:49 -0300

    fix

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Mappings/GetProductRequestProfile.cs
@@ -0,0 +1,15 @@
+﻿using Ambev.DeveloperEvaluation.Application.Products.CreateProduct;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings
+{
+    public class GetProductRequestProfile : Profile
+    {
+        public GetProductRequestProfile() {
+            CreateMap<CreateProductCommand, Product>();
+            CreateMap<Product, CreateProductResult>();
+
+        }
+    }
+}

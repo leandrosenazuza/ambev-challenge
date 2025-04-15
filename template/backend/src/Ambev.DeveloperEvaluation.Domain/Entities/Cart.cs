1a8f4679c84423c6937cb277402c2dfee9c3a1af
Leandro Zuza <lendro.zuza@connectbr.dev>
2025-04-15 07:59:39 -0300

    tested migrations succefully

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Cart.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Cart
+    {
+        [Column("id")]
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Column("user_id")]
+        [Required]
+        public int UserId { get; set; }
+
+        [Column("date")]
+        [Required]
+        public DateTime Date { get; set; }
+
+        //[NotMapped] 
+        public List<CartProduct> Products { get; set; } = [];
+
+        public Cart()
+        {
+        }
+    }
+}
